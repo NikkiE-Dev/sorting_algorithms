84d91c48db577f9231fb359578f64fc6003e6993
NikkiE-Holberton <78047506+NikkiE-Holberton@users.noreply.github.com>
2021-08-27 11:00:52 -0500

    Making file betty compliant

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,58 +9,58 @@
 
 void swapping(listint_t *frstnode, listint_t *secnode)
 {
-    secnode->prev = frstnode->prev;
+	secnode->prev = frstnode->prev;
 
-    if (secnode->prev)
-    secnode->prev->next = secnode;
-    frstnode->prev = secnode;
-    frstnode->next = secnode->next;
+	if (secnode->prev)
+	secnode->prev->next = secnode;
+	frstnode->prev = secnode;
+	frstnode->next = secnode->next;
 
-    if (frstnode->next)
-    frstnode->next->prev = frstnode;
-    secnode->next = frstnode;
+	if (frstnode->next)
+	frstnode->next->prev = frstnode;
+	secnode->next = frstnode;
 }
 
 /**
  * insertion_sort_list - sorting algorithm used on doubly linked list
  *
- * @list: the list created 
+ * @list: the list created
  */
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *curr, *temp, *fllw;
+	listint_t *curr, *temp, *fllw;
 
-    if (list == NULL)
-    {
-        return;
-    }
-    
-    curr = *list;
-    temp = curr->next;
+	if (list == NULL)
+	{
+		return;
+	}
 
-    while (temp)
-    {
-        if (curr->n > temp->n)
-        {
-            swapping(curr, temp);
-            if (!temp->prev)
-            *list = temp;
-            print_list(*list);
-            fllw = temp->prev;
+	curr = *list;
+	temp = curr->next;
 
-            while (fllw && fllw->n > temp->n)
-            {
-                swapping(fllw, temp);   
-                if (!temp->prev)
-                *list = temp;
-                print_list(*list);
-                fllw = temp->prev; 
-            }
-        }
-        
-        else
-        curr = temp;
-        temp = curr->next;
-    }
+	while (temp)
+	{
+		if (curr->n > temp->n)
+		{
+			swapping(curr, temp);
+			if (!temp->prev)
+			*list = temp;
+			print_list(*list);
+			fllw = temp->prev;
+
+			while (fllw && fllw->n > temp->n)
+			{
+				swapping(fllw, temp);
+				if (!temp->prev)
+				*list = temp;
+				print_list(*list);
+				fllw = temp->prev;
+			}
+		}
+
+		else
+		curr = temp;
+		temp = curr->next;
+	}
 }
