ca16cae12d61fda99c7cbcb7f2ad60d4af06bf61
NikkiE-Holberton <78047506+NikkiE-Holberton@users.noreply.github.com>
2021-08-26 23:46:55 -0500

    Code using insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,69 @@
+#include "sort.h"
+
+/**
+ * swapping - had to seperate code
+ *
+ * @frstnode: first node used in function
+ * @secnode: second node used in function
+ */
+
+ void swapping(listint_t *frstnode, listint_t *secnode)
+ {
+     secnode->prev = frstnode->prev;
+            if (secnode->prev)
+            {
+                secnode->prev->next = secnode;
+                frstnode->prev = secnode;
+                frstnode->next = secnode->next;
+            }
+            if (frstnode->next)
+            {
+                frstnode->next->prev = frstnode;
+                secnode->next - frstnode;
+            }
+ }
+
+/**
+ * insertion_sort_list - sorting algorithm used on doubly linked list
+ *
+ * @list: the list created 
+ */
+
+ void insertion_sort_list(listint_t **list)
+ {
+    if (*list == NULL || list == NULL)
+    {
+        return;
+    }
+
+    listint_t *curr, *temp, *fllw;
+    
+    curr = *list;
+    temp = curr->next;
+
+    while (temp)
+    {
+        if (curr->n > temp->n)
+        {
+            swapping(curr, temp);
+            if (!temp->prev)
+            *list = temp;
+            print_list(*list);
+            fllw = temp->prev;
+
+            while (fllw && fllw->n > temp->n)
+            {
+                swapping(fllw, temp);
+                if (!temp->prev)
+                *list = temp;
+                print_list(*list);
+                fllw = temp->prev; 
+            }
+        }
+        
+        else
+        curr = temp;
+        tmp = curr->next;
+    }
+
+ }
